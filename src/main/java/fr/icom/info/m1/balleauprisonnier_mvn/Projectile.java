63af4877691d39baaf57e31869daba9b82bd1fad
Manu-RMT <mandu543@gmail.com>
2022-10-17 12:44:19 +0200

    maj code git

---
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Projectile.java
@@ -0,0 +1,15 @@
+package fr.icom.info.m1.balleauprisonnier_mvn;
+
+import java.awt.*;
+
+public class Projectile {
+
+    int vitesse;
+    String direction;
+    Image projectile;
+
+    Projectile(){
+        projectile = new Image("assets/ball.png");
+    }
+
+}
